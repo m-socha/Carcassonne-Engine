7a900b71fd552c9886210d4012296452de6a9c93
Michael Socha <msocha@uwaterloo.ca>
2019-06-08 15:00:08 -0400

    Set meeples vector back to using values.

--- src/players/player.cc
+++ src/players/player.cc
@@ -3,15 +3,6 @@
 using namespace std;
 
 Player::Player() {
-  for (int i = 0; i < Player::NUM_PLAYER_MEEPLES; ++i) {
-    Meeple* meeple = new Meeple(this);
-    meeples.push_back(meeple);
-  }
-}
-
-Player::~Player() {
-  for(vector<Meeple*>::iterator it = meeples.begin(); it != meeples.end(); ++it) {
-    Meeple* meeple = *it;
-    delete meeple;
-  }
+  Meeple meeple(this);
+  meeples.insert(meeples.end(), Player::NUM_PLAYER_MEEPLES, meeple);
 }
--- src/players/player.h
+++ src/players/player.h
@@ -7,11 +7,10 @@
 class Player {
 public:
   Player();
-  virtual ~Player();
 
 private:
   static const int NUM_PLAYER_MEEPLES = 7;
-  std::vector<Meeple*> meeples;
+  std::vector<Meeple> meeples;
 };
 
 #endif
