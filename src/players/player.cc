bd51bc719fc77e9b39491e9c5f53e9be6aed35d4
Michael Socha <msocha@uwaterloo.ca>
2019-06-08 15:47:24 -0400

    Add player name field.

--- src/players/player.cc
+++ src/players/player.cc
@@ -2,9 +2,13 @@
 
 using namespace std;
 
-Player::Player() {
+Player::Player(string name) : name(name) {
   Meeple meeple(this);
   meeples.insert(meeples.end(), Player::NUM_PLAYER_MEEPLES, meeple);
+}
+
+const string& Player::getName() const {
+  return this->name;
 }
 
 Meeple* Player::getMeeple(int index) {
--- src/players/player.h
+++ src/players/player.h
@@ -1,16 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <string>
 #include <vector>
 #include "meeple.h"
 
 class Player {
 public:
-  Player();
+  Player(std::string name);
+  const std::string& getName() const;
   Meeple* getMeeple(int index);
 
 private:
   static const int NUM_PLAYER_MEEPLES = 7;
+
+  std::string name;
   std::vector<Meeple> meeples;
 };
 
