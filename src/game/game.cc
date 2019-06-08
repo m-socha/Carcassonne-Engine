29ec6b9a88e18a2515d247b9b4992f7b2c4fa816
Michael Socha <msocha@uwaterloo.ca>
2019-06-08 15:43:18 -0400

    Fix iterator termination condition.

--- src/game/game.cc
+++ src/game/game.cc
@@ -5,7 +5,7 @@ using namespace std;
 Game::Game(vector<Player*> players) : players(players) {}
 
 Game::~Game() {
-  for (vector<Player*>::iterator it = players.begin(); it != it; ++it) {
+  for (vector<Player*>::iterator it = players.begin(); it != players.end(); ++it) {
     Player* player = *it;
     delete player;
   }
