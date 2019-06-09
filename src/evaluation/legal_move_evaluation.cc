d4412d392da977f81ec4cd3e261d738a218dcbf8
Michael Socha <msocha@uwaterloo.ca>
2019-06-09 19:09:17 -0400

    Add helper method to get adjacent tiles.

--- src/evaluation/legal_move_evaluation.cc
+++ src/evaluation/legal_move_evaluation.cc
@@ -2,14 +2,17 @@
 
 using namespace std;
 
-static bool exists_adjacent_tile(const TileGrid& grid, int x, int y) {
-  vector<boost::optional<PlacedTile>> adjacent_tiles = {
+static vector<boost::optional<PlacedTile>> get_adjacent_tiles(const TileGrid& grid, int x, int y) {
+  return {
     grid.getTile(x - 1, y),
     grid.getTile(x + 1, y),
     grid.getTile(x, y - 1),
     grid.getTile(x, y + 1)
   };
+}
 
+static bool exists_adjacent_tile(const TileGrid& grid, int x, int y) {
+  vector<boost::optional<PlacedTile>> adjacent_tiles = get_adjacent_tiles(grid, x, y);
   return std::any_of(adjacent_tiles.begin(), adjacent_tiles.end(),
     [](boost::optional<PlacedTile> tile){
       return tile.is_initialized();
